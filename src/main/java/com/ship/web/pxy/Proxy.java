8ff05ca35fc6ac09b05dc5711a148f71ec6ceb92
maybehan <maybehans@gmail.com>
2019-11-07 18:27:04 +0900

    2019-11-07 18:26

--- src/main/java/com/ship/web/pxy/Proxy.java
+++ src/main/java/com/ship/web/pxy/Proxy.java
@@ -22,57 +22,64 @@ import lombok.Data;
 @Data
 @Lazy
 public class Proxy {
-	private int pageNum, pageSize, startRow, endRow;
+	private int totalCount, startRow, endRow, pageCount, pageNum, pageSize, startPage, endPage, blockCount, blockNum,
+			nextBlock, prevBlock;
 	private boolean existPrev, existNext;
 	private String search;
 	private final int BLOCK_SIZE = 5;
-	@Autowired Printer p;
-	@Autowired ArticleMapper articleMapper;
-	
+	@Autowired
+	Printer p;
+	@Autowired
+	ArticleMapper articleMapper;
+
 	@SuppressWarnings("unused")
 	public void paging() {
-		ISupplier<String> s = ()-> articleMapper.countArticle();
-		int totalCount = Integer.parseInt(s.get());
-		System.out.println("프록시 안에서 찍은 전체글 갯수: "+totalCount);
-		int pageCount = (totalCount % pageSize != 0) ? (totalCount / pageSize)+1 : totalCount / pageSize;
-		startRow = (pageNum-1)*pageSize;
-		endRow = (pageNum==pageCount) ? totalCount -1 : startRow + pageSize -1;
-		int blockCount = (pageCount % BLOCK_SIZE != 0) ? (pageCount / BLOCK_SIZE)+1 : pageCount / BLOCK_SIZE;
-		int blockNum = (pageNum - 1) / BLOCK_SIZE;
-		int startPage = blockNum * BLOCK_SIZE + 1;
-		int endPage = ((blockNum + 1) != blockCount) ? startPage + (BLOCK_SIZE -1) : pageCount;
+		ISupplier<String> s = () -> articleMapper.countArticle();
+		totalCount = Integer.parseInt(s.get());
+		System.out.println("프록시 안에서 찍은 전체글 갯수: " + totalCount);
+		pageCount = (totalCount % pageSize != 0) ? (totalCount / pageSize) + 1 : totalCount / pageSize;
+		startRow = (pageNum - 1) * pageSize;
+		endRow = (pageNum == pageCount) ? totalCount - 1 : startRow + pageSize - 1;
+		blockCount = (pageCount % BLOCK_SIZE != 0) ? (pageCount / BLOCK_SIZE) + 1 : pageCount / BLOCK_SIZE;
+		blockNum = (pageNum - 1) / BLOCK_SIZE;
+		startPage = blockNum * BLOCK_SIZE + 1;
+		endPage = ((blockNum + 1) != blockCount) ? startPage + (BLOCK_SIZE - 1) : pageCount;
 		existPrev = blockNum != 0;
 		existNext = (blockNum + 1) != blockCount;
+		nextBlock = startPage + BLOCK_SIZE;
+		prevBlock = startPage - BLOCK_SIZE;
+
 	}
+
 	public int integer(String param) {
 		Function<String, Integer> f = s -> Integer.parseInt(s);
 		return f.apply(param);
 	}
-	public List<?> crawl(Map<?,?> paramMap){
-		String url = "http://"+paramMap.get("site")+"/";
-		p.accept("넘어온 URL \n"+url);
+
+	public List<?> crawl(Map<?, ?> paramMap) {
+		String url = "http://" + paramMap.get("site") + "/";
+		p.accept("넘어온 URL \n" + url);
 		List<String> proxyList = new ArrayList<>();
 		proxyList.clear();
 		try {
-			Connection.Response response = Jsoup.connect(url)
-			                                    .method(Connection.Method.GET)
-			                                    .execute();
+			Connection.Response response = Jsoup.connect(url).method(Connection.Method.GET).execute();
 			Document document = response.parse();
 			String text = document.html();
-			//String text = document.text();
-			p.accept("크롤링한 텍스트 \n"+text);
+			// String text = document.text();
+			p.accept("크롤링한 텍스트 \n" + text);
 			proxyList.add(text);
-			
+
 		} catch (Exception e2) {
 			e2.printStackTrace();
 		}
-		
+
 		return proxyList;
 	}
+
 	public int random(int a, int b) {
-		BiFunction<Integer, Integer, Integer> f =(t,u)->(int)(Math.random()*(u-t))+t;
+		BiFunction<Integer, Integer, Integer> f = (t, u) -> (int) (Math.random() * (u - t)) + t;
 		return f.apply(a, b);
-		
+
 	}
-	
+
 }
